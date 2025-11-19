f7594ad649d4308bfce6e4b170ad14ea6f476bb2
Ray <abisuthans.business@gmail.com>
2025-11-19 11:08:06 -0500

    Create skeleton html

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [CSV, setCSV] = useState("");
+
+  const json_example = [
+    {
+      key: "value",
+      key2: "value2",
+    },
+    {
+      key: "value3",
+      key2: "value4",
+    },
+  ];
+
+  const csv_example = "Key,Key2\n" + "value,value2\n" + "value3,value4\n";
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>JSON -&gt; CSV</h1>
+      <form id="json-form">
+        <textarea
+          name="json"
+          rows="10"
+          cols="50"
+          placeholder={JSON.stringify(json_example)}
+        ></textarea>
+        <br/>
+        <button type="submit">Convert</button>
+      </form>
+      <textarea
+        name="csv"
+        rows="10"
+        cols="50"
+        placeholder={csv_example}
+      ></textarea>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
